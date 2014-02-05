4894c803dc2400ca22db89731fe172fedb7d784d
John Lynch <john@rigelgroupllc.com>
2014-02-05 15:36:56 -0800

    Implement server rendering

--- lib/react/rails/railtie.rb
+++ lib/react/rails/railtie.rb
@@ -5,6 +5,20 @@ module React
     class Railtie < ::Rails::Railtie
       config.react = ActiveSupport::OrderedOptions.new
 
+      # Sensible defaults. Can be overridden in application.rb
+      config.react.variant = ::Rails.env.development? ? :development : :production
+      config.react.addons = false
+      # Server-side rendering
+      config.react.max_renderers = 10
+      config.react.timeout = 20 #seconds
+      config.react.react_js = lambda {File.read(::Rails.application.assets.resolve('react.js'))}
+      config.react.component_filenames = ['components.js']
+
+      # Watch .jsx files for changes in dev, so we can reload the JS VMs with the new JS code.
+      initializer "react_rails.add_watchable_files" do |app|
+        config.watchable_files.concat Dir["#{app.root}/app/assets/javascripts/**/*.jsx*"]
+      end
+
       # run after all initializers to allow sprockets to pick up react.js and
       # jsxtransformer.js from end-user to override ours if needed
       config.after_initialize do |app|
@@ -12,32 +26,47 @@ module React
         # We want to include different files in dev/prod. The unminified builds
         # contain console logging for invariants and logging to help catch
         # common mistakes. These are all stripped out in the minified build.
-        if variant = app.config.react.variant || ::Rails.env.test?
-          variant ||= :development
-          addons = app.config.react.addons || false
 
-          # Copy over the variant into a path that sprockets will pick up.
-          # We'll always copy to 'react.js' so that no includes need to change.
-          # We'll also always copy of JSXTransformer.js
-          tmp_path = app.root.join('tmp/react-rails')
-          filename = 'react' + (addons ? '-with-addons' : '') + (variant == :production ? '.min.js' : '.js')
-          FileUtils.mkdir_p(tmp_path)
-          FileUtils.cp(::React::Source.bundled_path_for(filename),
-                       tmp_path.join('react.js'))
-          FileUtils.cp(::React::Source.bundled_path_for('JSXTransformer.js'),
-                       tmp_path.join('JSXTransformer.js'))
-          app.assets.prepend_path tmp_path
+        # Copy over the variant into a path that sprockets will pick up.
+        # We'll always copy to 'react.js' so that no includes need to change.
+        # We'll also always copy of JSXTransformer.js
+        tmp_path = app.root.join('tmp/react-rails')
+        filename = 'react' + (config.react.addons ? '-with-addons' : '') + (config.react.variant == :production ? '.min.js' : '.js')
+        FileUtils.mkdir_p(tmp_path)
+        FileUtils.cp(::React::Source.bundled_path_for(filename),
+                     tmp_path.join('react.js'))
+        FileUtils.cp(::React::Source.bundled_path_for('JSXTransformer.js'),
+                     tmp_path.join('JSXTransformer.js'))
+        app.assets.prepend_path tmp_path
 
-          # Allow overriding react files that are not based on environment
-          # e.g. /vendor/assets/react/JSXTransformer.js
-          dropin_path = app.root.join("vendor/assets/react")
-          app.assets.prepend_path dropin_path if dropin_path.exist?
+        # Allow overriding react files that are not based on environment
+        # e.g. /vendor/assets/react/JSXTransformer.js
+        dropin_path = app.root.join("vendor/assets/react")
+        app.assets.prepend_path dropin_path if dropin_path.exist?
 
-          # Allow overriding react files that are based on environment
-          # e.g. /vendor/assets/react/react.js
-          dropin_path_env = app.root.join("vendor/assets/react/#{variant}")
-          app.assets.prepend_path dropin_path_env if dropin_path_env.exist?
+        # Allow overriding react files that are based on environment
+        # e.g. /vendor/assets/react/react.js
+        dropin_path_env = app.root.join("vendor/assets/react/#{config.react.variant}")
+        app.assets.prepend_path dropin_path_env if dropin_path_env.exist?
+
+        # Server Rendering
+        # Concat component_filenames together for server rendering
+        config.react.components_js = config.react.component_filenames.map do |filename|
+          ::Rails.application.assets[filename].to_s
+        end.join(";")
+
+        do_setup = lambda do
+          cfg = ::Rails.application.config.react
+          React::Renderer.setup!( cfg.react_js.call, cfg.components_js,
+                                {:size => cfg.size, :timeout => cfg.timeout})
         end
+
+        do_setup.call
+
+        # Reload the JS VMs in dev when files change
+        ActionDispatch::Reloader.to_prepare(&do_setup)
+
+
       end
     end
   end
---
+++ lib/react/renderer.rb
@@ -0,0 +1,54 @@
+require 'connection_pool'
+
+module React
+  class Renderer
+
+    cattr_accessor :pool
+
+    def self.setup!(react_js, components_js, args={})
+      args.assert_valid_keys(:size, :timeout)
+      @@react_js = react_js
+      @@components_js = components_js
+      @@pool.shutdown{} if @@pool
+      @@pool = ConnectionPool.new(:size => args[:size]||10, :timeout => args[:timeout]||20) { self.new }
+    end
+
+    def self.render(component, args={})
+      @@pool.with do |renderer|
+        renderer.render(component, args)
+      end
+    end
+
+    def self.combined_js
+      @@combined_js ||= <<-CODE
+        var global = global || this;
+        #{@@react_js};
+        React = global.React;
+        #{@@components_js};
+      CODE
+    end
+
+    def context
+      @context ||= ExecJS.compile(self.class.combined_js)
+    end
+
+    def render(component, args={})
+      # Support both 0.8 and 0.9 API (with and without callback)
+      jscode = <<-JS
+        function() {
+          var cb_html = null;
+          var html = React.renderComponentToString(#{component}(#{args.to_json}), function(s){cb_html = s});
+          return cb_html || html;
+        }()
+      JS
+      context.eval(jscode).html_safe
+    # What should be done here? If we are server rendering, and encounter an error in the JS code,
+    # then log it and continue, which will just render the react ujs tag, and when the browser tries
+    # to render the component it will most likely encounter the same error and throw to the browser
+    # console for a better debugging experience.
+    rescue ExecJS::ProgramError => e
+      ::Rails.logger.error "[React::Renderer] #{e.message}"
+    end
+
+  end
+end
